69a1a765b8919683a188f73ef40381d3deb9347d
fernando-freire <fernando.freire@elo7.com>
2022-02-10 12:57:03 -0300

    create basic fastapi server

---
+++ server/api.py
@@ -0,0 +1,42 @@
+from typing import Optional
+import pickle
+import json
+import pandas as pd
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+# Post route for product categorization
+@app.post("/category")
+async def get_category(request: Request):
+
+    try:
+        prodct = json.loads(request.body)
+    except ValueError:
+        return await json.dumps({"status": 1, "info": "request failed."})
+
+    if prodct.get("products") is None:
+        return await jsonify({"error": "missing products field"})
+
+    products_list = prodct.get("products")
+
+    for t in products_list:
+        if t.get("title") is None:
+            return await jsonify({"error": "missing products title field"})
+
+    with open("model.pkl", "rb") as f:
+        text_clf = pickle.load(f)
+
+    df = pd.DataFrame(products_list)
+    df["text"] = df["concatenated_tags"] + " " + df["title"]
+    df = df.fillna(" ")
+    categories_list = text_clf.predict(df["text"])
+    return await jsonify({"categories": categories_list.tolist()})
