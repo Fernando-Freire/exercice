1dac7e3542ff0098fb11a9bffedef409bfcaf704
fernando-freire <fernando.freire@elo7.com>
2022-03-07 15:42:08 -0300

    wip novo teste para tipos de modelos

--- server/api.py
+++ server/api.py
@@ -1,18 +1,32 @@
 from typing import Optional
-import pickle
+
+import mlflow.pyfunc
+import nltk.stem
+from gensim.utils import simple_preprocess
+
+mlflow.set_tracking_uri("http://mlflow_server:5000")
+
 import pandas as pd
-import os
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from fastapi import FastAPI
+
 
 class Product(BaseModel):
     title: str
     concatenated_tags: str
 
+
 class Products(BaseModel):
     products: list[Product]
 
+
+nltk.download("rslp")
+
 app = FastAPI()
+
+stemmer = nltk.stem.RSLPStemmer()
+
 
 @app.get("/")
 async def read_root():
@@ -20,18 +34,48 @@ async def read_root():
 
 
 # Post route for product categorization
-@app.post("/category")
-async def get_category(products: Products):
-    model = os.getenv("MODEL_PATH")
-    print(products)
+@app.post("/category/{model_type}", status_code=200)
+async def get_category(model_type: str, products: Products):
+    implemented_model_types = ["tfidf", "bow", "Word2Vec", "Doc2Vec"]
+    if model_type not in implemented_model_types:
+        raise HTTPException(status_code=404, detail="Model Type Not Impllemented")
 
-    with open(model, "rb") as f:
-        text_clf = pickle.load(f)
+    ## Registered model names : "model_type"+"category"
+    category_list = [
+        "Bebê",
+        "Bijuterias e Jóias",
+        "Decoração",
+        "Lembrancinhas",
+        "Outros",
+        "Papel e Cia",
+    ]
 
-    df = pd.DataFrame(products_list)
-    df["text"] = df["concatenated_tags"] + " " + df["title"]
-    df = df.fillna(" ")
-    categories_list = text_clf.predict(df["text"])
-    return  {"categories": categories_list.tolist()}
+    model_name_prefix = model_type + "-DecisionTreeClass-"
 
+    print(products.dict()["products"])
+    text = []
+    title = []
+    for item in products.dict()["products"]:
+        text.append(item["title"] + " " + item["concatenated_tags"])
+        title.append(item["title"])
 
+    print(text)
+
+    df = pd.DataFrame(text, columns=["text"])
+
+    df["tokenized_text"] = [simple_preprocess(line, deacc=True) for line in df["text"]]
+    df["stemmed_tokens"] = [
+        [stemmer.stem(word) for word in tokens] for tokens in df["tokenized_text"]
+    ]
+
+    product_category = pd.DataFrame(title, columns=["Product"])
+    for category in category_list:
+        model_name = model_name_prefix + category
+        print("model_name is " + model_name)
+        model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
+
+        result = model.predict(df)
+
+        product_category[category] = [x == 1 for x in result]
+
+    return {"categories": product_category}
--- server/test_api.py
+++ server/test_api.py
@@ -1,27 +1,54 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+
 from api import app
 
-
-
 client = TestClient(app)
+
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello":"World"}
+    assert response.json() == {"Hello": "World"}
+
+
+def test_get_category_wrong_model():
+    data_json = {"products": [{"title": "string", "concatenated_tags": "string"}]}
+    response = client.post("/category/dingding", data_json)
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Model Type Not Impllemented"
+
+
+def test_get_category_wrong_fields():
+    data_json = {
+        "products": [
+            {
+                "title": "Cinto de Artes Marciais",
+                "concatenated_tags": "cinto artes-marciais roupa tecido",
+            },
+            {"title": "Carrinho de Bebê", "km": "10000"},
+            {"concatenated_tags": "Quadro de Parede", "preço": "3p"},
+            {"cor": "FF2200", "preço": "3097"},
+        ]
+    }
+    response = client.post("/category/bow", data_json)
+    assert response.status_code == 422
+    # assert len(response.json()["categories"]) == 2
 
 
 def test_get_category():
     data_json = {
-            "products": [
-                {
-                    "title": "Cinto de Artes Marciais",
-                    "concatenated_tags": "cinto artes-marciais roupa tecido",
-                },
-                {"title": "Carrinho de Bebê"},
-            ]
-        }
-    response = client.post("/category", data_json)
+        "products": [
+            {
+                "title": "Cinto de Artes Marciais",
+                "concatenated_tags": "cinto artes-marciais roupa tecido",
+            },
+            {
+                "title": "Quadro de plantas Tropicais",
+                "concatenated_tags": "quadro parede moldura decoração",
+            },
+        ]
+    }
+    response = client.post("/category/bow", data_json)
     assert response.status_code == 200
-    #assert len(response.json()["categories"]) == 2
+    # assert len(response.json()["categories"]) == 2
